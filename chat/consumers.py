87b13312ec44febc274ef2559e16d9df11b7cb7f
zak132465 <zakos123456@gmail.com>
2022-05-11 00:23:10 +0200

    sr

---
+++ chat/consumers.py
@@ -0,0 +1,9 @@
+import imp
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatRoomConsumer(AsyncWebsocketConsumer):
+    async def connett(self):
+        
+        await(
+            
+        )
---
+++ chat/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {}
+    return render(request, 'chat/index.html', context)
+
+def room(request, room_name):
+    context = {'room_name':room_name}
+    return render(request, 'chat/room.html', context)
